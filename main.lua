bb1c3aa1cd462c24865bcf99c9059fafc82d7c26
JFonS <joan.fonssanchez@gmail.com>
2016-01-31 11:08:59 +0100

    fixed bug

--- main.lua
+++ main.lua
@@ -11,23 +11,12 @@ Kill = require "kill"
 
 Score = 0
 
-local fish = {}
-
 function love.load()
     Gamestate.registerEvents()
-    --Gamestate.switch(Intro)
-    Gamestate.switch(Game)
+    Gamestate.switch(Intro)
+    --Gamestate.switch(Game)
 
     love.mouse.setVisible(false)
     love.mouse.setGrabbed(true)
-end
-
-function drawMouse() 
-    love.graphics.draw(emitter,love.mouse.getX(), love.mouse.getY(),10,10)
-end
-
-
-function love.draw()
-
 end
 
